60bbb9aa9883ed01600546a4526c0f931331dbd8
DomagojVrgoc <domagojvrgoc@gmail.com>
2018-08-06 17:52:19 -0400

    a

--- contract.sol
+++ contract.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.18;
 
-contract StelaHash{
+contract GovChain{
 
 	mapping (bytes32 => bytes32) public roots;
 	mapping (bytes32 => bytes32) public blocks_chain;
@@ -8,7 +8,7 @@ contract StelaHash{
     bytes32 public last_block_hash;
     address pubic publisher_address;
 
-	function StelaHash() public {
+	function GovChain() public {
         publisher_address = msg.sender;
         last_block_hash = 1;
     }
@@ -17,7 +17,7 @@ contract StelaHash{
         assert(block.timestamp - timestamps[last_block_hash] > 20 * 3600000);
 		assert(msg.sender == publisher_address);
         // Concatenate the previous block's hash and the new Merkle root.
-        new_block_hash = sha256(last_block_hash * 2 ** 32 + merkle_root);
+        bytes32 new_block_hash = sha256(last_block_hash * 2 ** 32 + merkle_root);
        	assert(roots[new_block_hash] != 0);
         roots[new_block_hash] = merkle_root;
         blocks_chain[new_block_hash] = last_block_hash;
