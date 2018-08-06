f5975e0c651e188502c72ab995acad560be353ce
Martin Ugarte <martin@martinugarte.com>
2018-08-06 23:05:26 +0200

    new version of contract

--- contract.sol
+++ contract.sol
@@ -2,22 +2,26 @@ pragma solidity ^0.4.18;
 
 contract StelaHash{
 
-	mapping (bytes32 => bytes32) public roots_chain;
+	mapping (bytes32 => bytes32) public roots;
+	mapping (bytes32 => bytes32) public blocks_chain;
     mapping (bytes32 => uint) public timestamps;
-    bytes32 public last_hash;
+    bytes32 public last_block_hash;
     address pubic publisher_address;
 
 	function StelaHash() public {
         publisher_address = msg.sender;
-        last_hash = 1;
+        last_block_hash = 1;
     }
 
-	function add_root(bytes32 root_hash) public {
-        assert(block.timestamp - timestamps[last_hash] > 20 * 3600000);
+	function add_root(bytes32 merkle_root) public {
+        assert(block.timestamp - timestamps[last_block_hash] > 20 * 3600000);
 		assert(msg.sender == publisher_address);
-       	assert(roots_chain[root_hash] != 0);
-        timestamps[root_hash] = block.timestamp;
-        roots_chain[root_hash] = last_hash; 
-        last_hash = root_hash;
+        // Concatenate the previous block's hash and the new Merkle root.
+        new_block_hash = sha256(last_block_hash * 2 ** 32 + merkle_root);
+       	assert(roots[new_block_hash] != 0);
+        roots[new_block_hash] = merkle_root;
+        blocks_chain[new_block_hash] = last_block_hash;
+        timestamps[new_block_hash] = block.timestamp;
+        last_block_hash = new_block_hash;
 	}
 }
